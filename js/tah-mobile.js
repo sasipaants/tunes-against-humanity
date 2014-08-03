8fb9a566cb054639b8420504c76d75f7df1313a1
sasilukr <p.pandapower@gmail.com>
2014-08-02 20:46:55 -0700

    Adding cards for guess page

--- js/tah-mobile.js
+++ js/tah-mobile.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
-	getSongsAPI();
+	// getSongsAPI();
+	getPlayersAPI();
 });
 function getSongsAPI() {
 	getSongs(getSongsCallback);
@@ -9,6 +10,29 @@ function getSongsCallback(error, data) {
 	console.log("Songs Error: " + JSON.stringify(error));
 	console.log("Songs Response: " + JSON.stringify(data));
 	var listMusic = $('#list-music');
-	var listDiv = $("<li class='list-music-item'>hi</li>");
+	var listDiv = $("<li class='list-music-item' onclick='selectSongAPI(\"hi\")'>hi</li>");
 	listMusic.append(listDiv);
+}
+
+function getPlayersAPI() {
+	getPlayers(getPlayersCallback);
+}
+
+function getPlayersCallback(error, data) {
+	console.log("Players Error: " + JSON.stringify(error));
+	console.log("Players Response: " + JSON.stringify(data));	
+}
+
+function selectSongAPI(song) {
+	// call selectSong(title, artist, twitterId);
+	$.mobile.changePage( '#waiting', { transition: 'slide'} );
+
+}
+
+function selectSong(title, artist, twitterId) {
+
+}
+
+function saveUser(twitter) {
+
 }
