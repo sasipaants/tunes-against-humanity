0e37b3d87d51d0cf072df0963e0fa6cd850fe897
Lita Cho <lita.cho@gmail.com>
2014-08-03 15:15:39 -0700

    Fix to 3 users

--- js/firebase.js
+++ js/firebase.js
@@ -231,7 +231,7 @@ function waitForNextRound() {
 
     var currentGameRef = root.child("games").limit(1);
     currentGameRef.on('child_added', function(snapshot) {
-        if(snapshot.child('players').numChildren() < 4 ) {
+        if(snapshot.child('players').numChildren() < 3 ) {
             //console.log("numplayers="+snapshot.child('players').numChildren());
             console.log("Next round starting.");
 //            window.location = 'index.html';
